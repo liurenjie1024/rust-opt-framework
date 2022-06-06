568857ebe11627e630f9b50db67a05bf5de353c5
Renjie Liu <liurenjie2008@gmail.com>
2022-06-06 16:28:39 +0800

    Add tet

--- src/datafusion_poc/rule.rs
+++ src/datafusion_poc/rule.rs
@@ -43,4 +43,106 @@ impl OptimizerRule for DFOptimizerAdapterRule {
   }
 }
 
+#[cfg(test)]
+mod tests {
+  use std::sync::Arc;
+  use datafusion::arrow::datatypes::Schema;
+  use datafusion::common::ToDFSchema;
+  use datafusion::datasource::empty::EmptyTable;
+  use datafusion::execution::context::ExecutionProps;
+  use datafusion::logical_expr::col;
+  use datafusion::logical_plan::{LogicalPlan, LogicalPlanBuilder};
+  use datafusion::optimizer::optimizer::OptimizerRule;
+  use serde_json::Value;
+  use crate::datafusion_poc::rule::DFOptimizerAdapterRule;
+  use crate::rules::{PushLimitOverProjectionRule, PushLimitToTableScanRule, RemoveLimitRule};
+  use datafusion::logical_plan::plan::{TableScan as DFTableScan, DefaultTableSource};
+
+  #[ignore]
+  #[test]
+  fn test_limit_push_down() {
+    let schema = {
+      let json = r#"{
+                "fields": [
+                    {
+                        "name": "c1",
+                        "nullable": false,
+                        "type": {
+                            "name": "utf8"
+                        },
+                        "children": []
+                    },
+                    {
+                        "name": "c2",
+                        "nullable": false,
+                        "type": {
+                            "name": "utf8"
+                        },
+                        "children": []
+                    }
+                ],
+                "metadata": {}
+            }"#;
+      let value: Value = serde_json::from_str(json).unwrap();
+      let schema = Schema::from(&value).unwrap();
+      Arc::new(schema)
+    };
+
+
+    // Construct datafusion logical plan
+    let df_logical_plan = {
+      let source = Arc::new(DefaultTableSource::new(Arc::new(EmptyTable::new(Arc::new
+          ((&*schema).clone().into())))));
+
+      let df_scan = DFTableScan {
+        table_name: "t1".to_string(),
+        source,
+        projection: None,
+        projected_schema:
+        (&*schema).clone().to_dfschema_ref().unwrap(),
+        filters: vec![],
+        limit: None,
+      };
+
+      LogicalPlanBuilder::from(LogicalPlan::TableScan(df_scan))
+          .limit(10).unwrap()
+          .project(vec![col("c1")]).unwrap()
+          .limit(5).unwrap()
+          .build().unwrap()
+    };
+
+    let optimized_plan = {
+      let rule = DFOptimizerAdapterRule {
+        rules: vec![
+          PushLimitOverProjectionRule::new().into(),
+          RemoveLimitRule::new().into(),
+          PushLimitToTableScanRule::new().into(),
+        ]
+      };
+
+      rule.optimize(&df_logical_plan, &ExecutionProps::new()).unwrap()
+    };
+
+    let expected_plan = {
+      let source = Arc::new(DefaultTableSource::new(Arc::new(EmptyTable::new(Arc::new
+          ((&*schema).clone().into())))));
+
+      let df_scan = DFTableScan {
+        table_name: "t1".to_string(),
+        source,
+        projection: None,
+        projected_schema: (&*schema).clone().to_dfschema_ref().unwrap(),
+        filters: vec![],
+        limit: Some(5),
+      };
+
+      LogicalPlanBuilder::from(LogicalPlan::TableScan(df_scan))
+          .project(vec![col("c1")]).unwrap()
+          .build().unwrap()
+    };
+
+    assert_eq!(format!("{:?}", expected_plan), format!("{:?}", optimized_plan));
+  }
+}
+
 
